cc9856461a86fc53e01f1b9d7cb661625f10ba84
Vlad <nik0late384@gmail.com>
2026-05-29 02:25:07 +0400

    Updated user credentials

--- src/test/java/tests/CreateUserTest.java
+++ src/test/java/tests/CreateUserTest.java
@@ -24,8 +24,8 @@ public class CreateUserTest {
     public void createUser() {
         User user = new User();
         user.setUsername(RandomUtils.getRandomAlphabeticString());
-        user.setFirstName(RandomUtils.getRandomAlphabeticString());
-        user.setLastName(RandomUtils.getRandomAlphabeticString());
+        user.setFirstName("Vladislav");
+        user.setLastName("Zenin");
         user.setEmail(RandomUtils.getRandomAlphabeticString());
         user.setPassword(RandomUtils.getRandomAlphabeticString());
         user.setPhone(RandomUtils.getRandomAlphabeticString());
